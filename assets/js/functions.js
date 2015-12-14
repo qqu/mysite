c2f18d82487246cd536ffd69dd9c9b5d75bfed5b
Qu <qing.qu@ARSMDBE2220702.local>
2015-12-14 17:53:15 -0500

    Add style and animation for gallery item, fix z-index issue which cause hover failure

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,18 +5,24 @@
 
 $(function(){
   var header = $("header"),
-yOffset = 0,
-triggerPoint = 150;
-$(window).scroll(function(){
-  yOffset = $(window).scrollTop();
+  yOffset = 0,
+  triggerPoint = 150;
+  $(window).scroll(function(){
+    yOffset = $(window).scrollTop();
+    if(yOffset >= triggerPoint){
+      header.addClass("site-header-min");
+    }else{
+      header.removeClass("site-header-min");
+    }
+  });
 
-  if(yOffset >= triggerPoint){
-    header.addClass("site-header-min");
-  }else{
-    header.removeClass("site-header-min");
-  }
-
+  //gallery image animation  
+  $('.project-post a').on('mouseenter',function() {
+    $(this).find('.image-wrapper').addClass('animated jello').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',function() {
+      $(this).removeClass('animated jello');
+    });
+  });
 
 });
+//end of doc loading
 
-});
