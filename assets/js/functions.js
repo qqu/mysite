f722e3e2d859f5066edc848ded83d8f180aaee75
Qu <qing.qu@ARSMDBE2220702.local>
2015-12-21 17:22:49 -0500

    add resume section and parallax

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,46 +1,46 @@
 /* 
-   Description: Animates the header upon scroll
+Description: Animates the header upon scroll
 
-*/
+ */
 
 $(function(){
 
     var header = $(".front header"),
-    yOffset = 0,
-    triggerPoint = 150;
-    $(window).scroll(function(){
-        yOffset = $(window).scrollTop();
-        //header scroll effect  
-        if(yOffset >= triggerPoint){
-            header.addClass("site-header-min");
-        }else{
-          header.removeClass("site-header-min");    
-        }
-        
-        //front page skills fadein
-        if(yOffset >= 300){
-            $('.skills-left').removeClass('hide').addClass('animated flipInX');
-            $('.skills-right').removeClass('hide').addClass('animated flipInX');
-        }
+        yOffset = 0,
+            triggerPoint = 150;
+            $(window).scroll(function(){
+                yOffset = $(window).scrollTop();
+                //header scroll effect  
+                if(yOffset >= triggerPoint){
+                    header.addClass("site-header-min");
+                }else{
+                    header.removeClass("site-header-min");    
+                }
 
-        $('section[data-type="background"]').each(function() {
-        var $bgObj = $(this);
-        var yPos = -(yOffset / $bgObj.data('speed'));
-        var bgP = '50% ' + yPos + 'px';
-        $bgObj.css("background-position", bgP);    
-        }); 
-    });
-    
-    $("body:not('.front')").find("header").addClass("site-header-min").removeClass("site-header");
+                //front page skills fadein
+                if(yOffset >= 240){
+                    $('.skills-left').removeClass('invisible').addClass('animated flipInX');
+                    $('.skills-right').removeClass('invisible').addClass('animated flipInX');
+                }
 
-    //gallery image animation  
-    /*
-    $('.project-post a').on('mouseenter',function() {
-        $(this).find('.image-wrapper').addClass('animated pulse').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',function() {
-            $(this).removeClass('animated pulse');
-        });
-    });
-    */
+                $('section[data-type="background"]').each(function() {
+                    var $bgObj = $(this);
+                    var yPos = -(yOffset / $bgObj.data('speed'));
+                    var bgP = '50% ' + yPos + 'px';
+                    $bgObj.css("background-position", bgP);    
+                }); 
+            });
+
+            $("body:not('.front')").find("header").addClass("site-header-min").removeClass("site-header");
+
+            //gallery image animation  
+            /*
+               $('.project-post a').on('mouseenter',function() {
+               $(this).find('.image-wrapper').addClass('animated pulse').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',function() {
+               $(this).removeClass('animated pulse');
+               });
+               });
+             */
 
 });
 //end of doc loading
