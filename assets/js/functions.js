56931222fc485145fc566c3fc16488f4e9d35ac9
Qing Qu <quqing11@gmail.com>
2015-12-22 00:59:34 -0500

    fix multiple parallax issue, add button to resume section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,46 +1,56 @@
 /* 
-Description: Animates the header upon scroll
+   Description: Animates the header upon scroll
 
- */
+*/
 
 $(function(){
 
     var header = $(".front header"),
-        yOffset = 0,
-            triggerPoint = 150;
-            $(window).scroll(function(){
-                yOffset = $(window).scrollTop();
-                //header scroll effect  
-                if(yOffset >= triggerPoint){
-                    header.addClass("site-header-min");
-                }else{
-                    header.removeClass("site-header-min");    
-                }
+    yOffset = 0,
+    triggerPoint = 150;
+    $(window).scroll(function(){
+        yOffset = $(window).scrollTop();
+        //header scroll effect  
+        if(yOffset >= triggerPoint){
+            header.addClass("site-header-min");
+        }else{
+            header.removeClass("site-header-min");    
+        }
 
-                //front page skills fadein
-                if(yOffset >= 240){
-                    $('.skills-left').removeClass('invisible').addClass('animated flipInX');
-                    $('.skills-right').removeClass('invisible').addClass('animated flipInX');
-                }
+        //front page skills fadein
+        if(yOffset >= 240){
+            $('.skills-left').removeClass('invisible').addClass('animated flipInX');
+            $('.skills-right').removeClass('invisible').addClass('animated flipInX');
+        }
 
-                $('section[data-type="background"]').each(function() {
-                    var $bgObj = $(this);
-                    var yPos = -(yOffset / $bgObj.data('speed'));
-                    var bgP = '50% ' + yPos + 'px';
-                    $bgObj.css("background-position", bgP);    
-                }); 
-            });
+        $('section[data-type="background"]').each(function() {
+            var $bgObj = $(this);
+            var distance = $bgObj.offset().top - yOffset;
+            var yPos = -(yOffset / $bgObj.data('speed')) ;
+            var bgP = '50% ' + yPos + 'px';
 
-            $("body:not('.front')").find("header").addClass("site-header-min").removeClass("site-header");
+            $bgObj.css("background-position", bgP);    
+        }); 
+    });
 
-            //gallery image animation  
-            /*
-               $('.project-post a').on('mouseenter',function() {
-               $(this).find('.image-wrapper').addClass('animated pulse').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',function() {
-               $(this).removeClass('animated pulse');
-               });
-               });
-             */
+    $("body:not('.front')").find("header").addClass("site-header-min").removeClass("site-header");
+
+    //gallery image animation  
+    /*
+       $('.project-post a').on('mouseenter',function() {
+       $(this).find('.image-wrapper').addClass('animated pulse').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',function() {
+       $(this).removeClass('animated pulse');
+       });
+       });
+       */
+
+    $('.resume-view-btn').hover(
+        function() {
+            $(this).addClass("animated jello");
+        }, function() {
+            $(this).removeClass("animated jello");
+        });
+
 
 });
 //end of doc loading
