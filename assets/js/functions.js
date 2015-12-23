51e2693cb7a33aff4ed555ff17a85a764807d1e5
Qu <qing.qu@ARSMDBE2220702.local>
2015-12-23 11:24:35 -0500

    add stellar js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -23,7 +23,7 @@ $(function(){
             $('.skills-right').removeClass('invisible').addClass('animated flipInX');
         }
 
-
+/*
         $('section[data-type="background"]').each(function() {
             var $bgObj = $(this);
             var distance = $bgObj.offset().top - yOffset;
@@ -32,7 +32,7 @@ $(function(){
                     
             $bgObj.css("background-position", bgP);    
         });
- 
+ */
     });
 
     $("body:not('.front')").find("header").addClass("site-header-min").removeClass("site-header");
@@ -51,6 +51,11 @@ $(function(){
             $(this).addClass("animated jello");
         }, function() {
             $(this).removeClass("animated jello");
+        });
+
+        $(window).stellar({
+             horizontalScrolling: false,
+             horizontalOffset: 0
         });
 
 
