88e972d98573b38f35bd7ccda515bda992dd5f8b
Qu <qing.qu@ARSMDBE2220702.local>
2015-12-28 17:35:20 -0500

    front page anchor, modify skill section style

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,64 +1,71 @@
 /* 
-   Description: Animates the header upon scroll
+Description: Animates the header upon scroll
 
-*/
+ */
 
 $(function(){
 
     var header = $(".front header"),
-    yOffset = 0,
-    triggerPoint = 150;
-    $(window).scroll(function(){
-        yOffset = $(window).scrollTop();
-        //header scroll effect  
-        if(yOffset >= triggerPoint){
-            header.addClass("site-header-min");
-        }else{
-            header.removeClass("site-header-min");    
-        }
+        yOffset = 0,
+            triggerPoint = 150;
+            $(window).scroll(function(){
+                yOffset = $(window).scrollTop();
+                //header scroll effect  
+                if(yOffset >= triggerPoint){
+                    header.addClass("site-header-min");
+                }else{
+                    header.removeClass("site-header-min");    
+                }
 
-        //front page skills fadein
-        if(yOffset >= 240){
-            $('.skills-left').removeClass('invisible').addClass('animated flipInX');
-            $('.skills-right').removeClass('invisible').addClass('animated flipInX');
-        }
+                //front page skills fadein
+                if(yOffset >= 240){
+                    $('.skills-left').removeClass('invisible').addClass('animated flipInX');
+                    $('.skills-right').removeClass('invisible').addClass('animated flipInX');
+                }
 
-/*
-        $('section[data-type="background"]').each(function() {
-            var $bgObj = $(this);
-            var distance = $bgObj.offset().top - yOffset;
-            var yPos = (yOffset / $bgObj.data('speed')) ;
-            var bgP = '50% ' + (distance - yPos) + 'px';
-                    
-            $bgObj.css("background-position", bgP);    
-        });
- */
-    });
+                /*
+                   $('section[data-type="background"]').each(function() {
+                   var $bgObj = $(this);
+                   var distance = $bgObj.offset().top - yOffset;
+                   var yPos = (yOffset / $bgObj.data('speed')) ;
+                   var bgP = '50% ' + (distance - yPos) + 'px';
 
-    $("body:not('.front')").find("header").addClass("site-header-min").removeClass("site-header");
+                   $bgObj.css("background-position", bgP);    
+                   });
+                 */
+            });
 
-    //gallery image animation  
-    /*
-       $('.project-post a').on('mouseenter',function() {
-       $(this).find('.image-wrapper').addClass('animated pulse').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',function() {
-       $(this).removeClass('animated pulse');
-       });
-       });
-       */
-
-    $('.resume-view-btn').hover(
-        function() {
-            $(this).addClass("animated jello");
-        }, function() {
-            $(this).removeClass("animated jello");
-        });
-
-        $(window).stellar({
-             horizontalScrolling: false,
-             horizontalOffset: 0
-        });
+            $("body:not('.front')").find("header").addClass("site-header-min").removeClass("site-header");
 
 
+            $('.resume-view-btn').hover(function() {
+                    $(this).addClass("animated jello");
+                }, function() {
+                    $(this).removeClass("animated jello");
+            });
+
+            $(window).stellar({
+                horizontalScrolling: false,
+                horizontalOffset: 0
+            });
+
+            scrollNav();
 });
+
+function scrollNav() {
+    $('.site-nav a').click(function(){  
+        //Toggle Class
+        $(".active").removeClass("active");      
+        $(this).closest('a').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.'+theClass).parent('a').addClass('active');
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href')  ).offset().top
+        }, 200);
+        return false;
+    });
+    $('.scrollTop a').scrollTop();
+}
 //end of doc loading
 
