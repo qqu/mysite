f9380217606315776d9d9c5eefc852c88479dcb2
Qing Qu <quqing11@gmail.com>
2016-01-08 22:05:24 -0500

    add slick

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -52,6 +52,13 @@ $(function(){
             });
 
             scrollNav();
+            $('.your-class').slick({
+                slidesToShow: 2,
+                  slidesToScroll: 1,
+                    autoplay: true,
+                      autoplaySpeed: 2000,
+            });
+
 });
 
 function scrollNav() {
